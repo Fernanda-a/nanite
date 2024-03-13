e6c431b9434fd2d0039c3aede6be86f84f4e39cd
Fernanda-a <myfernam@gmail.com>
2024-03-12 22:03:52 -0300

    Build: Finishing Knowledge Page

--- src/arvora/_core/main.py
+++ src/arvora/_core/main.py
@@ -22,35 +22,51 @@ Changelog
 |   `Labase <http://labase.selfip.org/>`_ - `NCE <http://portal.nce.ufrj.br>`_ - `UFRJ <https://ufrj.br/>`_.
 
 """
-MENU_OPTIONS = tuple(zip("PROJETO CONHECIMENTO PESQUISA INTERAÇÃO LOGIN USER".split(),
-                     "bars-progress book book-medical hands-asl-interpreting right-to-bracket user".split()))
+#Então, basicamente ele está transformando essa string com esses nomes em duas listas de substrings e juntando elas com o zip, e depois, transformando elas em uma tupla dos elementos dessa junção das sublistas. Aqui tem as partes do menu. Uma tupla é uma sequência imutável de valores. A função zip combina duas listas, combina o primeiro elemento da lista 1 com o primeiro elememto da lista 2. A função slip faz com que a string se transforme em uma lista de substrings. 
+MENU_OPTIONS = tuple(zip("PROJETO CONHECIMENTO PESQUISA PERGUNTAS LOGIN USER TESTE".split(),
+                     "bars-progress book book-medical question right-to-bracket user".split()))
 
-
+#Aqui uma base de página é criado. 
 class SimplePage:
+    #Essa classe tem um dicionário de páginas
     PAGES = dict()
 
+    #Aqui está declarando os atributos da classe
     def __init__(self, brython, menu=MENU_OPTIONS, hero="none_hero"):
+        #os [] mostram um código condensado. Basicamente é:
+        # _menu = []
+        # for items, icons in menu:
+        #     _menu.append({"title": items, "icon": icons})
         _menu = [{"title": items, "icon": icons} for items, icons in menu]
         self.brython = brython
         self.hero_class = hero
         self.items = []
+        #aqui é a página recebando o menu (não achei informações sobre .navigator)
         self.page = self.hero(self.navigator(_menu))
+        #aqui é outro código condensado. Ele é:
+        # for item in self.items:
+        #     item.bind("click", self.link)
         [item.bind("click", self.link) for item in self.items]
 
+    #quando você clicar em algum lugar ele vai mostrar a página dentro do dicionario PAGES. Ele pega o id e tira todas as mini mudanças para botar qual página é (eu acho)
     def link(self, ev=None):
         ev.preventDefault()
         page = ev.target.id.strip("-")
         self.PAGES[page].show()
 
     def show(self):
+        #limpando o html primeiro
         self.brython.document["pydiv"].html = ""
+        #botando o page como filho da div pydiv
         _ = self.brython.document["pydiv"] <= self.page
+        #A parte de user vai receber o nome do usuário logado
         self.brython.document["_USER_-"].html = Arvora.ARVORA.current_user
 
     def build_body(self):
         return ()
 
     def hero(self, navigator):
+        #Aqui tem um lugar com várias divs. Div do header, do body e uma section com os dois.
         h = self.brython.html
         cnt = h.DIV(self.build_body(), Class="container has-text-centered pb-6 mgb-large")
         hby = h.DIV(cnt, Class="hero-body is-justify-content-center is-align-items-center")
@@ -85,7 +101,9 @@ class LandingPage(SimplePage):
         h = self.brython.html
         tt1 = h.P("A R V O R A", Class="title main-text has-text-weight-bold")
         tt2 = h.P("Brain Computational School", Class="title is-1 main-text")
-        return h.DIV((tt1, tt2))
+        #phr = phrase
+        phr = h.P("Seu lugar de pesquisas de neurociência!", Class='main-text title is-3')
+        return h.DIV((tt1, tt2, phr))
 
 
 class LoginPage(SimplePage):
@@ -117,55 +135,127 @@ class LoginPage(SimplePage):
         cls = h.DIV(form, Class="columns is-flex is-flex-direction-column")
         return cls
 
-class InteractionPage(SimplePage):
-    #Adicionando o init
-    def __init__(self, brython, menu=MENU_OPTIONS):
+class KnowledgePage(SimplePage):
+    def __init__(self, brython, menu = MENU_OPTIONS):
         super().__init__(brython, menu, hero="main_hero")
-        self.form = self.text = None
 
-    def click(self, ev=None):
-        _ = self
-        ev.preventDefault()
-        form = ev.target
-        # USER_OPTIONS = form.elements["username"].value
-        Arvora.ARVORA.user(form.elements["username"].value)
-        SimplePage.PAGES["_MAIN_"].show()
-
-        # self.brython.alert(form.elements["username"].value, form.elements["password"])
-        # print(self.login.value, self.passd.type)
-
-    #construindo a página em si
     def build_body(self):
         h = self.brython.html
+        usern = "Roberto"
+        userp = "@robertao_do_pix"
+        
+        #no card tem a foto do usuario o nome e arroba
+        #coloquei media_content pois n sei se haverão @ de usuario
 
-        #um botão para enviar o formulário
-        btn = h.BUTTON("Enviar", Class="button has-background-grey-light is-4 block is-fullwidth", type="submit")
 
-        #O campo onde as pessoas pode escrever o texto delas, esse monte de tag é o bulma. Ela tem os placeholders e o rows que é a quantidade padrão de linhas
-        self.text = h.TEXTAREA(Id="text", Class="textarea is-success has-fixed-size block mb-4 mt-0 has-background-grey has-text-success-light is-medium", rows='17', type="text", placeholder="Comece a escrever aqui!")
+        user_photo = h.FIGURE((h.IMG(src="https://res.cloudinary.com/ameo/image/upload/v1639144778/typocat_svbspx.png")), Class = "media-left image is-48x48")
 
-        #Aqui eu criei uma div para armazenar todos os componentes da página
-        div = h.DIV()
+        user_name = h.P(usern, Class = "title is-4")
+        user_prof = h.P(userp, Class = "subtitle is-6")
+        media_content = h.DIV((user_name, user_prof), Class = "media-content")
+        head = h.DIV((user_photo, media_content), Class = "header media ")
+
+        search_bar = h.FORM(h.DIV(h.INPUT(Id='local_search', Class='input is-white has-fixed-size block has-background-grey has-text-success-light mb-4 white-placeholder', placeholder='Pesquisar'), Class='column'))
+
+        # post_img = h.FIGURE(h.IMG(src="https://bulma.io/images/placeholders/256x256.png"), Class="card-image image is-4by3")
+        # post_buttons = (h.BUTTON("Comentar", Class = "button is-primary"),
+        #                 h.BUTTON("Perguntar", Class="button is-info"),
+        #                 h.BUTTON("Artigos Filhos", Class="button"))
+        # post = h.DIV((head, post_img, post_buttons), Class="column is-half is-offset-one-quarter card"
+
+        #exemplos de artigos
+        artigos = [{"title":"Artigo", "abstract": """Feeling unsure of my naked bodyStand back, watch it taking shapeWondering why I don't look like BarbieThey say boys like girls with a tiny waistNow my mama's preaching to make sure I'm pureBut I never really cared 'bout this shit beforeLook around the room to whoever wants me Got boys acting like they ain't seen skin beforeGot sent home to change 'cause my skirt is too short It's my fault, it's my fault 'cause I put icing on top Now, the boys want a taste of the strawberry shortcake That's my bad, that's my bad, no one told them not to grab Now, the boys want a taste of the strawberry shortcakeGotta make sure that my legs are shinyHot wax melting, burn my skinPeople all around me watching closely'Cause it's how I look and not what I thinkMikey's eyes seem to be g"""}, {"title":"Artigo 2", "abstract": "resumo 2"}, {"title":"Artigo 3", "abstract": "resumo 3"}]
+        #todos os artigos
+        toa = []
+
+        for a in artigos:
+            title = a["title"]
+            abstract = a["abstract"]
+
+            tit = h.P(title, Class ='title is-4')
+            abst = h.P(abstract, Class='text is-6')
+            post_buttons = h.DIV((h.BUTTON("Comentar", Class = "button is-primary"),
+                            h.BUTTON("Perguntar", Class="button is-info mx-4"),
+                            h.BUTTON("Artigos Filhos", Class="button")), Class='columns mx-4 mt-5')
             
-        #tit == titulo. Esse é o título da página
-        tit = h.P("Escreva seu artigo", Class='title is-2 block hero p-2 has-text-white incText')
+            toa.append(h.DIV((tit, abst, post_buttons), Class='box'))
+        
+        posts = h.DIV((search_bar, toa), Class="column body-columns")
 
-        #aut == autor. Aqui que a pessoa pode botar o nome dela ((só uma ideia inicial))
-        aut = h.INPUT(placeholder='Autor', Id='input', Class='input is-success has-fixed-size block has-background-grey has-text-success-light is-medium')
+        btn1 = h.BUTTON("Resumos", Class="button has-background-grey-light is-4 block is-fullwidth")
+        btn2 = h.BUTTON("Escreva seu artigo", Class="button has-background-grey-light is-4 block is-fullwidth")
+        side_tab = h.DIV((btn2, btn1), Class='column is-3')
 
-        #Aqui eu to adicionando tudo dentro da div, na ordem que eu quero que eles aparecam
-        div <= tit
-        div <= aut
-        div <= self.text
+        wrapper = h.DIV((side_tab, posts), Class="columns")
 
-        #aqui eu encapsulei a div com tudo e o botão em um formulário
-        form = h.FORM((div, btn), Class="column")
-        form.bind("submit", self.click)
+        return wrapper
 
-        #inte == interactions. aqui eu adicionei tudo isso em outra div
-        inte = h.DIV(form, Class="columns is-flex")
-        #Aqui eu to retornando a div com todos os elementos
-        return inte
+# class QuestionsPage(SimplePage):
+#     def __init__(self, brython, menu=MENU_OPTIONS):
+#         super().__init__(brython, menu, hero="main_hero")
+
+#     def build_body(self):
+#         h = self.brython.html
+        #vamos começar a criar a parte das perguntas. Essa página terá quadrados mostrando perguntas que terão um título, e a pergunta mais relevante. Eu ainda quero botar um fadding.
+        #tit para título
+        # a = artigos do site
+        # b = perguntas
+
+        # for i in a:
+        #     if b:
+        #         tit = titulo do artigo
+        #         qst = pergunta mais relavante
+        # #qst para question
+        # qst = 
+        
+    # #Adicionando o init
+    # def __init__(self, brython, menu=MENU_OPTIONS):
+    #     super().__init__(brython, menu, hero="main_hero")
+    #     self.form = self.text = None
+
+    # def click(self, ev=None):
+    #     _ = self
+    #     ev.preventDefault()
+    #     form = ev.target
+    #     # USER_OPTIONS = form.elements["username"].value
+    #     Arvora.ARVORA.user(form.elements["username"].value)
+    #     SimplePage.PAGES["_MAIN_"].show()
+
+    #     # self.brython.alert(form.elements["username"].value, form.elements["password"])
+    #     # print(self.login.value, self.passd.type)
+
+    # #construindo a página em si
+    # def build_body(self):
+    #     h = self.brython.html
+
+    #     #um botão para enviar o formulário
+    #     btn = h.BUTTON("Enviar", Class="button has-background-grey-light is-4 block is-fullwidth", type="submit")
+
+    #     #O campo onde as pessoas pode escrever o texto delas, esse monte de tag é o bulma. Ela tem os placeholders e o rows que é a quantidade padrão de linhas
+    #     self.text = h.TEXTAREA(Class="textarea is-success has-fixed-size block mb-4 mt-0 has-background-grey has-text-success-light is-medium white-placeholder", rows='17', type="text", placeholder="Comece a escrever aqui!")
+
+    #     #Aqui eu criei uma div para armazenar todos os componentes da página
+    #     div = h.DIV()
+            
+    #     #tit == titulo. Esse é o título da página
+    #     tit = h.P("Escreva seu artigo", Class='title is-2 block hero p-2 has-text-white incText')
+
+    #     #aut == autor. Aqui que a pessoa pode botar o nome dela ((só uma ideia inicial))
+    #     aut = h.INPUT(placeholder='Autor', Class='input is-success has-fixed-size block has-background-grey has-text-success-light is-medium white-placeholder')
+
+    #     #Aqui eu to adicionando tudo dentro da div, na ordem que eu quero que eles aparecam
+    #     div <= tit
+    #     div <= aut
+    #     div <= self.text
+
+    #     #aqui eu encapsulei a div com tudo e o botão em um formulário
+    #     form = h.FORM((div, btn), Class="column")
+    #     form.bind("submit", self.click)
+
+    #     #inte == interactions. aqui eu adicionei tudo isso em outra div
+    #     quest = h.DIV(form, Class="columns is-flex")
+    #     #Aqui eu to retornando a div com todos os elementos
+    #     return quest
 
 class Arvora:
     ARVORA = None
@@ -184,7 +274,8 @@ class Arvora:
         SimplePage.PAGES = {f"_{page}_": SimplePage(br) for page, _ in MENU_OPTIONS}
         SimplePage.PAGES["_MAIN_"] = LandingPage(br)
         SimplePage.PAGES["_LOGIN_"] = LoginPage(br)
-        SimplePage.PAGES['_INTERAÇÃO_'] = InteractionPage(br)
+       # SimplePage.PAGES['_PERGUNTAS_'] = QuestionsPage(br)
+        SimplePage.PAGES["_CONHECIMENTO_"] = KnowledgePage(br)
         _main = LandingPage(br)
         _main.show()
         return _main
